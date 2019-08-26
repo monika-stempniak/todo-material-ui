a20716a079421d5056abfc6faf99e772da16572e
Monika Stempniak <mstempniak@gmail.com>
2019-08-26 22:42:19 +0200

    Update App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 
 import { About, NotFound, Todos } from './pages';
 import App from './App';
 
 const history = createBrowserHistory();
 
-test('should render without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App component', () => {
+
+    test('should render without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test('should automatically redirect to /todos page', () => {
+      const { getByTestId } = render(<App />);
+
+      expect(getByTestId('todos-title').innerHTML).toMatch('Todos');
+    });
+
+    test('should navigate from /todos to /about page', () => {
+      const { getByTestId, getByText } = render(<App />);
+
+      fireEvent.click(getByText(/about/i), { button: 0 });
+      expect(getByTestId('about-title').innerHTML).toMatch('About');
+    });
+
+    test('should land on a not found page when route is wrong', () => {
+      history.push('/some-route');
+      const { getByTestId } = render(<App />);
+
+      expect(getByTestId('not-found-title').innerHTML).toMatch('Page not found');
+    });
 });
-
-test('should navigate from /todos to /about page', () => {
-  const { getByTestId, getByText } = render(<App />);
-  expect(getByTestId('todos-title').innerHTML).toMatch('Todos');
-  const leftClick = { button: 0 };
-  fireEvent.click(getByText(/about/i), leftClick);
-  expect(getByTestId('about-title').innerHTML).toMatch('About');
-})
-
-test('should land on a not found page when route is wrong', () => {
-  history.push('/some-route');
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('not-found-title').innerHTML).toMatch('Page not found');
-})
