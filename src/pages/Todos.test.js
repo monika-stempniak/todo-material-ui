1f6ed940b0ac4e67b20f5a0b8f8a50af2287e897
Monika Stempniak <mstempniak@gmail.com>
2019-08-27 19:46:09 +0200

    Test todos request error

--- src/pages/Todos.test.js
+++ src/pages/Todos.test.js
@@ -32,6 +32,9 @@ describe('Todos component', () => {
   beforeEach(() => {
     axios.get.mockResolvedValueOnce({data: todos});
   });
+  afterEach(() => {
+    axios.get.mockReset();
+  });
 
   it("matches snapshot", async () => {
     const { asFragment } = render(<Todos />);
@@ -43,14 +46,15 @@ describe('Todos component', () => {
   test('should render list of todos', async () => {
     const { getByTestId } = render(<Todos />);
     expect(getByTestId('todos-loading').innerHTML).toMatch('Loading...');
-  
+
     await waitForDomChange();
 
+    expect(axios.get).toHaveBeenCalledTimes(1);
     const todoList = getByTestId('todos-list');
     expect(todoList).toBeTruthy();
     expect(todoList.children.length).toBe(3);
   })
-  
+
   test('should allow adding new todos', async () => {
     const { getByTestId, debug, getByLabelText, getByText, getAllByTestId } = render(<Todos />);
 
@@ -68,5 +72,22 @@ describe('Todos component', () => {
     expect(todoList.children.length).toBe(4);
     expect(getByText(todo)).toBeTruthy();
     expect(getAllByTestId('todo')[0].innerHTML).toContain(todo);
+  });
+});
+
+describe('Todos component - error test', () => {
+  test('should display an error if request fails', async () => {
+    const error = 'Async error';
+    axios.get.mockRejectedValue(new Error(error));
+
+    const { container, queryByTestId } = render(<Todos />);
+
+    await waitForDomChange();
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(container).toHaveTextContent(`Something went wrong! ${error}`);
+
+    const todoList = document.querySelector('[data-testid="todos-list"]');
+    expect(todoList).not.toBeInTheDocument();
   });
 });
