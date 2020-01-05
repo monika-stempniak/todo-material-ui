c62fc956c603f6c491b2875d72f84d8e2f6f7495
Monika Stempniak <mstempniak@gmail.com>
2020-01-06 00:13:07 +0100

    Fix Todo unit tests

--- src/components/Todo.test.js
+++ src/components/Todo.test.js
@@ -1,50 +1,46 @@
-import React from 'react';
-import { render, fireEvent, waitForElement } from '@testing-library/react';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
 
-import { todos } from '../lib/todos-mock';
-import { Todo } from '.';
+import { Todo } from ".";
 
-const todo = todos[0];
+const todo = {
+  userId: 1,
+  id: 1,
+  title: "Buy milk",
+  completed: false
+};
 
 const props = {
   index: 1,
-  todo,
-}
+  todo
+};
 
-describe('Todo component', () => {
-  let completeTodo;
+describe("Todo component", () => {
+  let toggleTodo;
   let deleteTodo;
   beforeEach(() => {
-    completeTodo = jest.fn();
+    toggleTodo = jest.fn();
     deleteTodo = jest.fn();
   });
 
-  test('should allow to complete todo', async () => {
+  test("should allow to complete todo", async () => {
     const { getByTestId } = render(
-      <Todo
-        {...props}
-        completeTodo={completeTodo}
-        deleteTodo={deleteTodo}
-      />
+      <Todo {...props} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     );
 
-    fireEvent.click(getByTestId('complete'));
-    expect(completeTodo).toHaveBeenCalledTimes(1);
+    fireEvent.click(getByTestId("todo-buy-milk"));
+    expect(toggleTodo).toHaveBeenCalledTimes(1);
     expect(deleteTodo).toHaveBeenCalledTimes(0);
   });
 
-  test('should allow to delete todo', async () => {
+  test("should allow to delete todo", async () => {
     todo.completed = true;
     const { getByTestId } = render(
-      <Todo
-        {...props}
-        completeTodo={completeTodo}
-        deleteTodo={deleteTodo}
-      />
+      <Todo {...props} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     );
 
-    fireEvent.click(getByTestId('delete'));
+    fireEvent.click(getByTestId("delete-buy-milk"));
     expect(deleteTodo).toHaveBeenCalledTimes(1);
-    expect(completeTodo).toHaveBeenCalledTimes(0);
+    expect(toggleTodo).toHaveBeenCalledTimes(0);
   });
 });
